88e27109c8a562a2f0031b9eb66c948768226066
adonishenriquw <adonis.henrique21@gmail.com>
2023-05-22 22:31:17 -0300

    feat: setup routing-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationComponent } from './components/authentication/authentication.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "authentication", component: AuthenticationComponent},
+  {path: "dashboard", component: DashboardComponent},
+  {path: "", pathMatch: "full", redirectTo: "authentication"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
