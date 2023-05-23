7dac9dafea58863eaa28bc18ce7f7a26a712bd89
adonishenriquw <adonis.henrique21@gmail.com>
2023-05-22 22:23:22 -0300

    feat: create api-football services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthenticationComponent } from './components/authentication/authentication.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
